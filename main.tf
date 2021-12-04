f7156e16247304ea52aaf65e1525271b59f3f59c
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-12-04 17:44:59 -0300

    wip

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "kubeconfig" {
   source     = "github.com/getupcloud/terraform-module-kubeconfig?ref=main"
 
   cluster_name = module.cluster.name
-  command      = var.get_kubeconfig_command
+  command      = var.kubeconfig_get_kubeconfig_command
 }
 
 module "flux" {
@@ -17,5 +17,17 @@ module "flux" {
 
   git_repo       = var.flux_git_repo
   manifests_path = "./clusters/${var.name}/kind/manifests"
-
 }
+
+module "cronitor" {
+  source = "github.com/getupcloud/terraform-module-cronitor?ref=main"
+
+  cluster_name  = module.cluster.cluster_id
+  customer_name = var.customer
+  suffix        = "kind"
+  tags          = []
+  pagerduty_key = var.cronitor_pagerduty_key
+  api_key       = var.cronitor_api_key
+  api_endpoint  = module.cluster.cluster_endpoint
+}
+
