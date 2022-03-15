fb4293ceb8b1d036a7b45427b9837101a092ef57
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-03-15 16:11:49 -0300

    Add providers and requires

---
+++ providers.tf
@@ -0,0 +1,16 @@
+provider "kubernetes" {
+  config_path            = var.use_kubeconfig ? var.kubeconfig_filename : null
+  host                   = var.use_kubeconfig ? null : local.api_endpoint
+  cluster_ca_certificate = var.use_kubeconfig ? null : local.certificate_authority_data
+  client_certificate     = var.use_kubeconfig ? null : local.client_certificate_data
+  client_key             = var.use_kubeconfig ? null : local.client_key_data
+}
+
+provider "kubectl" {
+  load_config_file       = var.use_kubeconfig ? null : false
+  host                   = var.use_kubeconfig ? null : local.api_endpoint
+  cluster_ca_certificate = var.use_kubeconfig ? null : local.certificate_authority_data
+  client_certificate     = var.use_kubeconfig ? null : local.client_certificate_data
+  client_key             = var.use_kubeconfig ? null : local.client_key_data
+  apply_retry_count      = 2
+}
